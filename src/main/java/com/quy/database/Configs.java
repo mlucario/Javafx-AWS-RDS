d098139dc0e8027fdf209baaf1a20b63b3e7103e
Quy Nguyen <52801407+mlucario88@users.noreply.github.com>
2019-10-31 16:33:29 -0700

    x
    x

--- src/main/java/com/quy/database/Configs.java
+++ src/main/java/com/quy/database/Configs.java
@@ -4,7 +4,7 @@ public class Configs {
 	protected static String dbHost="database-bizcom.cvrt884ix9ft.us-west-1.rds.amazonaws.com";
 	protected static String dbPort="3306";
 	protected static String dbUsername = "admin";
-	protected static String dbPassword = "1202Amazon";
+	protected static String dbPassword = "Bizcom1171";
 	protected static String dbName="database-bizcom";
 	
 	
