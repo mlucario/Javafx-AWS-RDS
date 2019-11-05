ed583fc1e9a6547f172a7db233bc8dd5528a335e
QuyNguyen <nhonquy@gmail.com>
2019-11-04 21:42:42 -0800

    Update SMCController.java

--- src/main/java/com/quy/bizcom/SMCController.java
+++ src/main/java/com/quy/bizcom/SMCController.java
@@ -9,6 +9,13 @@ import javafx.beans.value.ObservableValue;
 public class SMCController extends RecursiveTreeObject<SMCController> {
 
 	StringProperty controllerBarcode;
+	StringProperty controllerModel;
+
+	public SMCController(StringProperty controllerBarcode, StringProperty controllerModel) {
+		super();
+		this.controllerBarcode = controllerBarcode;
+		this.controllerModel = controllerModel;
+	}
 
 	public SMCController(String controllerBarcode) {
 		this.controllerBarcode = new SimpleStringProperty(controllerBarcode);
